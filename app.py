fd7dc7c53a63a04113a49484966f4610bd23ae48
vansh visariya <vanshvisariya1985@gmail.com>
2026-01-29 13:13:55 +0530

    add port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -10,5 +11,9 @@ def home():
 def about():
     return render_template('about.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
